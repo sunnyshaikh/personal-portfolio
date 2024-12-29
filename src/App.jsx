21fd93a303db169f3ac8ae4c1eec3fa251d9812d
AltafAlam Shaikh <gitforaltaf@gmail.com>
2024-12-29 13:15:53 +0530

    component separation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Stars from "./assets/stars.png";
 import Navbar from "./components/Navbar";
 import VerticalSocialIcons from "./components/VerticalSocialIcons";
 import VeritcalComponent from "./components/VeritcalComponent";
-import AnimatedBlurredBg from "./components/ui/AnimatedBlurredBg";
-import GradientButton from "./components/ui/GradientButton";
-import { FaLocationArrow } from "react-icons/fa";
 import VerticalMail from "./components/VerticalMail";
+import Hero from "./pages/Hero";
+import Education from "./pages/Education";
 
 const App = () => {
   return (
@@ -17,38 +15,12 @@ const App = () => {
       <VeritcalComponent position={"left"}>
         <VerticalSocialIcons />
       </VeritcalComponent>
-      <section
-        className="hero h-screen relative"
-        style={{ background: `url(${Stars})` }}
-      >
-        {/* blurry background */}
-        <AnimatedBlurredBg />
 
-        <div className="relative flex flex-col items-center text-center justify-center h-full m-auto">
-          {/* large title */}
-          <h1 className="text-4xl sm:text-7xl font-bold">
-            I'm Altaf Shaikh
-            <br />
-            <span className="bg-gradient-to-r from-indigo-200 to-indigo-500 text-transparent bg-clip-text">
-              Software Developer.
-            </span>
-          </h1>
+      {/* Hero */}
+      <Hero />
 
-          {/* sub title */}
-          <p className="mt-6 text-lg sm:text-xl">
-            Transforming Concepts into Seamless User Experiences.
-          </p>
-
-          {/* cta button */}
-          <div className="mt-16">
-            <GradientButton>
-              <span className="flex gap-2 items-center">
-                Show my work <FaLocationArrow />
-              </span>
-            </GradientButton>
-          </div>
-        </div>
-      </section>
+      {/* Education */}
+      <Education />
     </main>
   );
 };
